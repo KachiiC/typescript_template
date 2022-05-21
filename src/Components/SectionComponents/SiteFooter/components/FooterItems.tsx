26a7508bf39f61615137ec374bdd06692f9c16db
KachiiC <kacheokcheong@gmail.com>
2022-05-22 00:24:05 +0100

    added logic

--- src/Components/SectionComponents/SiteFooter/components/FooterItems.tsx
+++ src/Components/SectionComponents/SiteFooter/components/FooterItems.tsx
@@ -1,71 +1,60 @@
-// CSS
-import MediaQuery from "react-responsive"
 // PROPS
-import { 
-    footerCopyrightProps,
-    footerHeadingProps,
-    footerSectionDataProps
-} from "../SiteFooterProps"
+import {
+  footerCopyrightProps,
+  footerHeadingProps,
+  footerSectionDataProps
+} from "../SiteFooterProps";
 // TOOLS
-import { RenderLogic } from "Tools/FunctionTools"
-import SiteIcon from "Tools/SiteIcon"
-import { SiteLink } from "Tools/LinkTools"
-import { StringJoin } from "Tools/StringTools"
+import { RenderLogic } from "Tools/FunctionTools";
+import SiteIcon from "Tools/SiteIcon";
+import { SiteLink } from "Tools/LinkTools";
+import { StringJoin } from "Tools/StringTools";
 
-export const FooterHeading = (props: footerHeadingProps) => RenderLogic(<h3>{props.heading}</h3>, "")
+export const FooterHeading = (props: footerHeadingProps) =>
+  RenderLogic(<h3>{props.heading}</h3>, "");
 
 export const FooterLogos = (props: footerSectionDataProps) => {
+  const { icon, link } = props;
 
-    const { icon, link } = props
+  const iconImage = <SiteIcon type={RenderLogic(icon, "html")} size="2x" />;
 
-    const iconImage = (
-        <SiteIcon 
-            type={RenderLogic(icon, 'html')} 
-            size="2x"
-        />
-    )
-
-    return (
-        <div className="site-col-1">
-            <SiteLink
-                link={RenderLogic(link, "")}
-                placeholder={iconImage}
-                type="external"  
-            />
-        </div>
-    )
-}
+  return (
+    <div className="site-col-1">
+      <SiteLink
+        link={RenderLogic(link, "")}
+        placeholder={iconImage}
+        type="external"
+      />
+    </div>
+  );
+};
 
 export const FooterLinks = (props: footerSectionDataProps) => {
+  const { external_link, link, span, title } = props;
 
-    const { external_link, link, span, title } = props
-
-    const FooterLink = (props: { input: any }) => (
-        <div className={`site-col-${props.input} footer-link`}
-            key={title}
-        >
-            <SiteLink
-                link={`${RenderLogic(external_link, `/${link}`)}`}
-                type={external_link ? "external" : "local"}
-                placeholder={StringJoin(title, "_", "")} 
-            />
-        </div>
-    )
-
-    return (
-        <>
-            <MediaQuery minWidth={1000}>
-                <FooterLink input={1} />
-            </MediaQuery>
-            <MediaQuery maxWidth={1000}>
-                <FooterLink input={span} />
-            </MediaQuery>
-        </>
-    )
-}
-
-export const FooterCopyRight = (props: footerCopyrightProps) => (
-    <div className="footer-copyright">
-        &copy; <i>Designed by {props.name} {props.year}</i>
+  const FooterLink = (props: { input: any }) => (
+    <div className={`site-col-${props.input} footer-link`} key={title}>
+      <SiteLink
+        link={`${RenderLogic(external_link, `/${link}`)}`}
+        type={external_link ? "external" : "local"}
+        placeholder={StringJoin(title, "_", "")}
+      />
     </div>
-)
+  );
+
+  return (
+    <>
+      <FooterLink input={1} />
+      <FooterLink input={span} />
+    </>
+  );
+};
+
+export const FooterCopyRight = ({name, year}: footerCopyrightProps) => (
+  <div className="footer-copyright">
+    &copy;{" "}
+    <i>
+      Designed by {name} {year}
+    </i>
+  </div>
+);
